72bc6610f5ddb4e57a8d33775cfb816043c17868
minhdevs <104881854+minhdevs@users.noreply.github.com>
2022-07-05 09:29:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,45 @@ function getList() {
     .then(function (response) {
       var html = response.map(function (e, i) {
         return `
-            <div class="item ${
-              e.completed == true ? "completed" : "undone"
-            }" idValue="${e.id}">
-                <h3 class="times">${
-                  e.times
-                } ${e.date}  <input type="checkbox" ${e.completed == true ? "checked=true" : ""}" /></h3>
-                <h4 class="title">${e.title}</h4>
-                <p class="description">${e.description}</p>
-            </div>
-            `;
+        <div class="item  ${
+          e.completed == true ? "completed" : "undone"
+        }" idValue="${e.id}">
+          <b class="times">
+            ${
+              e.times
+            } ${e.date} <input class="status" type="checkbox" ${e.completed == true ? "checked=true" : ""} />
+          </b>
+          <div class="times" style="float: right">
+            <button>Edit</button> <button>Remove</button>
+          </div>
+          <h4 class="title">${e.title}</h4>
+          <p class="description">${e.description}</p>
+        </div>`;
       });
 
       LContainer.innerHTML = html.join("");
+
+      var items = document.querySelectorAll(".item");
+
+      items.forEach((element) => {
+        // Revome button
+        var remove = element.childNodes[3].childNodes[3];
+        remove.onclick = function () {
+          element.remove();
+        };
+
+        // Set status
+        var input = element.childNodes[1].childNodes[1];
+        element.childNodes[1].childNodes[1].onclick = function () {
+          if (input.checked) {
+            element.classList.add("completed");
+            element.classList.remove("undone");
+          } else {
+            element.classList.add("undone");
+            element.classList.remove("completed");
+          }
+        };
+      });
     });
 }
 
